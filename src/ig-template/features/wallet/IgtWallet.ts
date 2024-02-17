2cdab39235098b288a9efb2f1b2a3be54b7eeb14
Jenny Aronow <56799827+Jennserene@users.noreply.github.com>
2024-02-17 15:55:35 -0500

    Add gainMultipleCurrencies() to wallet (#17)
    * Added ability to gain multiple currencies with tests.
    
    * Added public to the only method in IgtWallet.ts that did not have it

--- src/ig-template/features/wallet/IgtWallet.ts
+++ src/ig-template/features/wallet/IgtWallet.ts
@@ -48,9 +48,18 @@ export class IgtWallet extends IgtFeature {
   }
 
   /**
+   * Gain the currency amount from the specified currencies.
+   */
+  public gainMultipleCurrencies(currencies: Currency[]): void {
+    for (const currency of currencies) {
+      this.gainCurrency(currency);
+    }
+  }
+
+  /**
    * Return true if all currencies are valid and the player has the specified amount.
    */
-  hasCurrencies(costs: Currency[]): boolean {
+  public hasCurrencies(costs: Currency[]): boolean {
     for (const cost of costs) {
       if (!this.hasCurrency(cost)) {
         return false;
