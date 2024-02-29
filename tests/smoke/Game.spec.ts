9c4db6e6a76e7855f6650657d885b82e27af3edf
Jenny Aronow <56799827+Jennserene@users.noreply.github.com>
2024-02-29 09:53:50 -0500

    Add `onTick` event to `IgtGame` (#26)
    * Added onTick event, moved creation of setInterval to it's own function startTickInterval(), changed function called to tick() which calls update() and dispatches the onTick event, pulled delta calculations out of update() and into tick(), added event subscription and tests to smoke test
    
    * adjusted variable names from event
    
    * Ran linting and added a test
    
    * Changed forceUpdate to fakeTick
    
    * Switched onTick event to a simple event

--- tests/smoke/Game.spec.ts
+++ tests/smoke/Game.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from 'vitest';
+import { beforeEach, describe, expect, test } from 'vitest';
 
 import { IgtGame } from '@/ig-template/IgtGame';
 import { IgtWallet } from '@/ig-template/features/wallet/IgtWallet';
@@ -20,31 +20,80 @@ class DummyGame extends IgtGame {
 
   protected readonly SAVE_KEY: string = 'dummy';
   protected readonly TICK_DURATION: number = 0.05;
+
+  public get tickDuration(): number {
+    return this.TICK_DURATION;
+  }
 }
 
-/**
- * This smoke test starts the game and runs 100 game ticks.
- * It fails if any exceptions are thrown.
- */
 describe('Game launch smoke test', () => {
-  const game = new DummyGame({
-    wallet: new IgtWallet(['money']),
-    settings: new IgtSettings(),
-    codes: new IgtRedeemableCodes(),
-    keyItems: new IgtKeyItems(),
-    specialEvents: new IgtSpecialEvents(),
-    statistics: new IgtStatistics(),
-    achievements: new IgtAchievements(),
+  let game: DummyGame;
+
+  beforeEach(() => {
+    game = new DummyGame({
+      wallet: new IgtWallet(['money']),
+      settings: new IgtSettings(),
+      codes: new IgtRedeemableCodes(),
+      keyItems: new IgtKeyItems(),
+      specialEvents: new IgtSpecialEvents(),
+      statistics: new IgtStatistics(),
+      achievements: new IgtAchievements(),
+    });
   });
+
+  const ticks: number = 100;
+
+  const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  /**
+   * This smoke test starts the game and runs 100 game ticks.
+   * It fails if any exceptions are thrown.
+   */
   test('smoke test', () => {
+    let tickCount = 0;
+    const unsub = game.onTick.sub((event: number) => {
+      tickCount++;
+      expect(event).toEqual(game.tickDuration);
+    });
+
     expect(() => {
       game.initialize();
       game.load();
       game.start();
 
-      for (let i = 0; i < 100; i++) {
-        game.forceUpdate(0.5);
+      for (let i = 0; i < ticks; i++) {
+        game.fakeTick(game.tickDuration);
       }
+
+      expect(tickCount).toBe(ticks);
     }).not.toThrow();
+
+    unsub();
+  });
+
+  test('starts and stops properly', async () => {
+    let tickCount = 0;
+    const unsub = game.onTick.sub((_sender, _event) => {
+      tickCount++;
+    });
+
+    game.initialize();
+    game.load();
+
+    const threeTicksMs = game.tickDuration * 3 * 1000;
+    await delay(threeTicksMs);
+    expect(tickCount).toBe(0);
+
+    game.start();
+    await delay(threeTicksMs);
+    expect(tickCount).toBeGreaterThan(0);
+
+    game.stop();
+    const tickCountAfterStop = tickCount;
+    expect(game.state).toBe('Stopped');
+    await delay(threeTicksMs);
+    expect(tickCount).toBe(tickCountAfterStop);
+
+    unsub();
   });
 });
